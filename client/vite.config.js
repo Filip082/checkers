d16eb45cdb0b9365091c899da8206eb35133aee1
Filip Kwidziński <fkwidzinski@student.agh.edu.pl>
2026-01-21 01:39:29 +0100

    Improve dev workflow

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,20 +6,15 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      // 1. Proxy dla zapytań API (fetch)
       '/api': {
-        target: 'http://localhost:3000', // Adres Twojego backendu
+        target: 'http://server:3000', // Adres Twojego backendu
         changeOrigin: true,
         secure: false
-        // Opcjonalnie: jeśli na backendzie nie masz prefixu '/api' w endpointach,
-        // użyj rewrite, aby go wyciąć przed wysłaniem do serwera:
-        // rewrite: (path) => path.replace(/^\/api/, '')
       },
       
-      // 2. Proxy dla Socket.io (ważne!)
       '/socket.io': {
-        target: 'http://localhost:3000',
-        ws: true, // Włącza wsparcie dla WebSocket
+        target: 'http://server:3000',
+        ws: true,
         changeOrigin: true
       }
     }
